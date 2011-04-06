aaf0aca256727ec62b6f8287a8ca86b4c551871f
Yuri Cherkasov <yuri.cherkasov.accounts@gmail.com>
2011-04-06 14:34:19 +0400

    ch 9 process groups

---
+++ unix/ch_09_procgroup/procgroup.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+/*
+ * Chapter  Stievens, Rago
+ */
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}

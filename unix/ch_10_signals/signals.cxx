e192712a6c22b3a335642e9364e1a540a5e026b4
Yuri Cherkasov <yuri.cherkasov.accounts@gmail.com>
2011-04-06 17:16:36 +0400

    ch 10 signals

---
+++ unix/ch_10_signals/signals.cxx
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+/*
+ * Chapter  Stievens, Rago
+ */
+
+int main(int argc, char* argv[]) {
+
+    try{
+        
+    }
+    catch(const std::exception& e){
+      cerr << e.what() << endl;
+    }
+    return 0;
+}

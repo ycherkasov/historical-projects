256191ac84dd5621c5cf32a6ba328fda48b9a19b
ycherkasov <ycherkasov@votvd-ycher.(none)>
2011-04-27 16:11:54 +0400

    pthread

---
+++ unix/ch_11_pthread/pthread.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdexcept>
+
+using std::string;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+/*
+ * Chapter 11 Stievens, Rago. Pthread.
+ *
+ */
+int main(){
+    return 0;
+}

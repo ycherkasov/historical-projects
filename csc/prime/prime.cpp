ef62c57f7d46f8c220b5e6719684c1b7f99da8c1
Yuri Cherkasov <yuri.cherkasov.accounts@gmail.com>
2011-05-29 19:17:47 +0400

    aks prime test

--- csc/prime/prime.cpp
+++ csc/prime/prime.cpp
@@ -1,5 +1,66 @@
+#include <stdio.h>
+#include <string.h>
 #include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <boost/lexical_cast.hpp>
+#include "primetest_ntl.h"
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+
+//template <typename Func>
+void get_permutations(std::string& numbers) {
+    std::sort(numbers.begin(), numbers.end());
+    do {
+        unsigned check_me = boost::lexical_cast<unsigned>(numbers);
+        if ((check_me % 2) == 1) {
+            // todo : threaded pool
+            //if(Func(check_me)){
+            aks_test aks(check_me);
+            if (aks()) {
+                cout << check_me << endl;
+            }
+
+        }
+    }
+    while (std::next_permutation(numbers.begin(), numbers.end()));
+}
 
 int main(int argc, char* argv[]) {
+
+
+    if (argc != 2) {
+        cout << "Usage: primetest <numbers>" << endl;
+    }
+
+#if 1
+    try {
+        string strnums(argv[1]);
+        //get_permutations<aks_test>(strnums);
+        get_permutations(strnums);
+
+        cout << endl;
+    }
+    catch (boost::bad_lexical_cast& e) {
+        cerr << e.what() << "(not a number?)" << endl;
+    }
+#else
+    string strnums(argv[1]);
+    unsigned check_me = boost::lexical_cast<unsigned>(strnums);
+    NTL::INIT_VAL_STRUCT t = {};
+    ntl_bigint bcheckme(t, check_me);
+    if (aks_test(bcheckme)) {
+        cout << check_me << " is prime" << endl;
+    }
+    else {
+        cout << check_me << " is composite" << endl;
+    }
+#endif
+
     return 0;
 }
+

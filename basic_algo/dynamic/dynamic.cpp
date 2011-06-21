29e3c5d0346acb30889626dfdb13d0d9cbf0c3b8
ycherkasov <ycherkasov@votvd-ycher.(none)>
2011-06-21 15:18:58 +0400

    dynamic

--- basic_algo/dynamic/dynamic.cpp
+++ basic_algo/dynamic/dynamic.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <set>
 #include <boost/lexical_cast.hpp>
+#include <boost/tokenizer.hpp>
 #include <iosfwd>
 
 using std::string;
@@ -11,9 +12,8 @@ using std::endl;
 
 /// Определите только один из дефайнов!!!
 
-#define _SIMPLE_
 //#define _MONEY_
-//#define _FROGS_
+#define _FROGS_
 
 /*
  * Задача о кузнечике - изветсная задача на динамическое программирование
@@ -39,53 +39,56 @@ class grasshopper {
 public:
 
     grasshopper(unsigned steps, frog_cols& frg, coins_cols& ccl)
-        :_steps(steps),way(){
+    : _steps(steps), way() {
 
-        // совсем стартовые условия
-        way[0].ways = 1;
-        way[1].ways = 1;
-        
-        // если кол-во шагов больше 2, надо поставить начальные условия
-        // (проинициализаировать первые STEPS ячеек)
-        if(steps > 2){
-            //cout << "debug steps > 2" << endl;
-            for(size_t i = 2 ; i < steps ; i++){
 
-#ifdef _FROG_
-                if(frg.find(i) != ccl.end()){
-                    way[i].ways = 0;
+        for (int i = 0;i < SIZE;i++) {
+
+            // инициализация стартовых условий
+            if (i < steps) {
+
+                // совсем стартовые условия
+                if (i < 2){
+                    way[i].ways = 1;
+                    cout << "debug: " << way[i].ways << endl;
                 }
                 else{
-                    way[i].ways = last_count(i, i);
+                    // если кол-во шагов больше 2, надо поставить начальные условия
+                    // (проинициализаировать первые STEPS ячеек)
+                    way[i].ways =  last_count(i, i) ;
                 }
-#endif
-                // например, 2-я ячейка, сложить последние 2 (0 и 1)
-                way[i].ways = last_count(i, i);
-#ifdef _MONEY_
-                if(ccl.find(i) != ccl.end()){
-                    way[i].money = ccl.at(i);
-                }
-#endif
             }
-        }
 
-        //cout << "debug initial" << endl;
+            // инициализация "лягушек"
+            if(frg.find(i) != frg.end()){
+                way[i].frog = true;
+                way[i].ways = 0;
+            }
+
+            // инициализация монет
+
+        }
+        
+        cout << "debug initial" << endl;
         print_path();
     }
 
-    void print_path(){
-        for(size_t i = 0 ; i < SIZE ; i++){
-            cout << '\t' << way[i].ways ;
+    void print_path() {
+        for (size_t i = 0;i < SIZE;i++) {
+            cout << '\t' << way[i].ways;
+            if (way[i].frog) cout << "F";
         }
         cout << endl;
     }
 
     /** @ brief Сложить предыдущие STEPS значений */
-    unsigned last_count(size_t index, size_t steps){
+    unsigned last_count(size_t index, size_t steps) {
         unsigned uns = 0;
+
         //cout << "debug index: " << index << endl;
-        for(size_t i = index - steps ; i < index ; i ++)
+        for (size_t i = index - steps;i < index;i++)
             uns += way[i].ways;
+
         //cout << "debug uns: " << uns << endl;
         return uns;
     }
@@ -94,8 +97,14 @@ public:
     unsigned path_count() {
 
         // первые STEPS-1 ячеек уже проинициализированы, начинаем со STEPS
-        for(size_t i = _steps ; i < SIZE ; i++){
-            way[i].ways = last_count(i, _steps);
+        for (size_t i = _steps;i < SIZE;i++) {
+
+            if (way[i].frog) {
+                way[i].ways = 0;
+            }
+            else {
+                way[i].ways = last_count(i, _steps);
+            }
         }
         return way[LAST].ways;
     }
@@ -103,16 +112,20 @@ public:
 private:
 
     /** @ brief Класс столбик*/
-    struct column{
-        column():ways(),money(){}
-        
+    struct column {
+
+        column() : ways(), money(), frog(false) { }
+
         /** @ brief Количество путей, чтобы допрыгать до столбика */
         unsigned ways;
 
         /** @ brief Количество монет, которые дают или отбирают */
         int money;
+
+        /** @brief Если ли на столбике лягушка */
+        bool frog;
     };
-    
+
     /// на сколько можем прыгать вперед
     unsigned _steps;
 
@@ -126,19 +139,54 @@ private:
     column way[SIZE];
 };
 
-void parse_frogs(){}
-void parse_coins(){}
+std::pair<int, int> string_token(const std::string & ip_port) {
+    // we should tokenize string "IP:PORT" into "IP" and "PORT"
+    typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+    boost::char_separator<char> sep("-");
+    tokenizer tokens(ip_port, sep);
+    std::vector<string> srvstrings(tokens.begin(), tokens.end());
+    std::pair<int, int> column;
+
+    // if tokenized sucessfully
+    if (srvstrings.size() == 2) {
+        column.first = boost::lexical_cast<int>(srvstrings.at(0));
+        column.second = boost::lexical_cast<int>(srvstrings.at(1));
+    }
+    else {
+        std::stringstream errstream;
+        errstream << "Error parse string " << ip_port;
+        throw std::runtime_error(errstream.str());
+    }
+    return column;
+}
+
+void parse_coins(std::map<int, int>& changeme, int argc, char* argv[]) {
+
+    for (int i = 2;i < argc;i++) {
+        changeme.insert(string_token(argv[i]));
+    }
+}
+
+void parse_frogs(std::set<int>& changeme, int argc, char* argv[]) {
+    std::cout << "debug " << argc << endl;
+    for (int i = 2;i < argc;i++) {
+        int f = boost::lexical_cast<int>(argv[i]);
+        std::cout << "Frog at " << f << " column" << endl;
+        changeme.insert(f);
+    }
+}
 
 int main(int argc, char* argv[]) {
 
 
     std::set<int> frogs;
     std::map<int, int> coins;
-    
+
 #ifdef _FROGS_
     if (argc < 3) {
         std::cout << "Usage: program <stepsize> <frognum1> <frognum2> ... " << std::endl;
     }
+    parse_frogs(frogs, argc, argv);
 #endif
 
 #ifdef _MONEY_
@@ -146,14 +194,6 @@ int main(int argc, char* argv[]) {
         std::cout << "Usage: program <stepsize> <frognum1> <column-count> ... " << std::endl;
     }
 #endif
-
-
-#ifdef _SIMPLE_
-    if (argc != 2) {
-        std::cout << "Usage: program <stepsize>" << std::endl;
-    }
-#endif
-
 
     try {
 
@@ -163,8 +203,12 @@ int main(int argc, char* argv[]) {
         g.print_path();
 
     }
-    catch (const boost::bad_lexical_cast& e) {}
-    catch (const std::exception& e) {}
-    
+    catch (const boost::bad_lexical_cast& e) {
+        std::cout << e.what();
+    }
+    catch (const std::exception& e) {
+        std::cout << e.what();
+    }
+
     return 0;
 }

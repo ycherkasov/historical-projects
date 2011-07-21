da48092a7fd1f0209b3b6c2f853a8a59f1ea20cc
ycherkasov <ycherkasov@votvd-ych.(none)>
2011-07-21 13:12:38 +0400

    template project migration to VS 2010

--- boost_ex/fusion/fusion.cpp
+++ boost_ex/fusion/fusion.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <boost/lexical_cast.hpp>
+#include <boost/fusion/sequence.hpp>
+#include <boost/fusion/include/sequence.hpp>
 
 int main(int argc, char* argv[]) {
-    
-    if (argc != 2) {
-        std::cout << "Usage: program <number>" << std::endl;
+
+    //    if (argc != 2) {
+    //        std::cout << "Usage: program <number>" << std::endl;
+    //    }
+
+    try {
+
     }
-    try{
+    catch (const boost::bad_lexical_cast& e) {
     }
-    catch(const boost::bad_lexical_cast& e){
-    }
-    catch(const std::exception& e){
+    catch (const std::exception& e) {
     }
     return 0;
 }
--- boost_ex/mpl/mpl.cpp
+++ boost_ex/mpl/mpl.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 #include <boost/lexical_cast.hpp>
 #include <boost/mpl/vector_c.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/assert.hpp>
+#include <boost/mpl/at.hpp>
 
 using namespace boost;
 using std::cout;
 using std::endl;
 
-struct S1{
+struct S1 {
     int a;
     int b;
 };
 
-struct S2{
+struct S2 {
     int a;
 };
 
-typedef boost::mpl::vector_c<int, 50, 0> type1_t;
-typedef boost::mpl::vector_c<int, 10, 5> type2_t;
+typedef mpl::vector<S1, mpl::int_<50>, mpl::int_<0> > types;
 
 int main(int argc, char* argv[]) {
-    
-//    if (argc != 2) {
-//        std::cout << "Usage: program <number>" << std::endl;
-//    }
-    try{
-        cout << mpl::plus<mpl::int_<2>, mpl::int_<3> >::type::value << endl;
+
+    try {
+        cout << mpl::plus<mpl::int_ < 2 >, mpl::int_ < 3 > >::type::value << endl;
+
+        BOOST_MPL_ASSERT((is_same< mpl::at_c<types, 0 >::type, S1 >));
+
+        cout << mpl::at_c<types, 1 >::type::value << endl;
     }
-    catch(const boost::bad_lexical_cast& e){
+    catch (const boost::bad_lexical_cast& e) {
     }
-    catch(const std::exception& e){
+    catch (const std::exception& e) {
     }
     return 0;
 }

838c2466c951d8ddc322fd5c942442a5b6f0ab9c
ycher <ycher@nmrx-vm4.(none)>
2011-09-06 19:40:49 +0400

    valgrind example

--- valgrind/val.cpp
+++ valgrind/val.cpp
@@ -67,7 +67,7 @@ int main(int argc, char* argv[]) {
             ("uninit",uninit)
             ("fpe",fpe);
 
-    if (argc != 2) {
+    if (argc < 3) {
         std::cout << "Usage: program memleak|oor|uninit|fpe" << std::endl;
     }
     try {

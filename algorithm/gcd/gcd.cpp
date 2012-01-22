b5ce72eb33f35bbcad5733a38c38c6dfc2c1de81
Yuri Cherkasov <yuri.cherkasov.accounts@gmail.com>
2012-01-22 20:18:18 +0300

    Levitin book exercises

--- algorithm/gcd/gcd.cpp
+++ algorithm/gcd/gcd.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+/// Greater common divisor implementation
+/// return : Greater common divisor for 'm' and 'n'
+
+unsigned gcd(const unsigned m, const unsigned n){
+    unsigned n_(n);
+    unsigned m_(m);
+
+    while(n_){
+        unsigned r = m_ % n_;
+        m_ = n_;
+        n_ = r;
+    }
+    return m_;
+}
+
+using namespace std;
+
 int main()
 {
-  return 0;
+    cout << "gcd(60, 24) = " << gcd(60, 24) << endl;
+    cout << "gcd(1838, 7855) = " << gcd(1838, 7855) << endl;
+    cout << "gcd(1, 17) = " << gcd(1, 17) << endl;
+    return 0;
 }

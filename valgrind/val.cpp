aff8f08ad59c6a8d9f76dae9ea2dfd05d5edaaa0
Firstname Lastname <yuri.cherkasov@gmail.com>
2012-04-17 12:53:37 +0400

    cachemiss profilig added

--- valgrind/val.cpp
+++ valgrind/val.cpp
@@ -33,11 +33,28 @@ void show_fpe(){
     
 }
 
+void show_cachemiss(){
+  long val1 = 100;
+  long telm01 = 0;
+  long telm31 = 0;
+  long telm21 = 0;
+  long telm11 = 0;
+  long elmg1[] = {1,5,2,6,3,2,4,66,7,4,9,0,3,7,5,0,1,4,2,3};
+  for (size_t i = 0; i < 20; i++) {
+    telm01 = elmg1[i];
+    telm31 = (telm01 << 3) ^ val1;
+    telm21 = (telm01 << 2) ^ (val1 >> 1);
+    telm11 = (telm01 << 1) ^ (val1 >> 2);
+  }
+  cout << telm01 << ' ' << telm31 << ' ' << telm21 << ' ' << telm11 << endl;
+}
+
 enum operation{
     memleak,
     oor,
     uninit,
-    fpe
+    fpe,
+    cachemiss
 };
 
 void execute_example(operation op){
@@ -54,11 +71,17 @@ void execute_example(operation op){
         case fpe:
             show_fpe();
             break;
+        case cachemiss:
+	    show_cachemiss();
+	    break;
         default:
             return;
     }
 }
 
+void show_usage(){
+  std::cout << "Usage: program memleak|oor|uninit|fpe|cache" << std::endl;
+}
 
 int main(int argc, char* argv[]) {
 
@@ -67,10 +90,12 @@ int main(int argc, char* argv[]) {
             = boost::assign::map_list_of("memleak",memleak)
             ("oor",oor)
             ("uninit",uninit)
-            ("fpe",fpe);
+            ("fpe",fpe)
+            ("cache", cachemiss);
 
-    if (argc < 3) {
-        std::cout << "Usage: program memleak|oor|uninit|fpe" << std::endl;
+    if (argc < 2) {
+      show_usage();
+      return 0;
     }
     try {
         typedef std::map<std::string, operation>::const_iterator map_cit;
@@ -80,7 +105,6 @@ int main(int argc, char* argv[]) {
             if ((it = program_mode.find(mode)) != program_mode.end()) {
                 execute_example((*it).second);
             }
-
         }
     }
     catch(const std::exception& e){

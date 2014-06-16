c779b4002fc991e50e451b67d167b084e43e940a
yuri cherkasov <yuri.cherkasov.accounts@gmail.com>
2014-06-16 10:31:49 +0400

    sort out about.txt

--- strauscpp3/ch_03_basics/iterators.cpp
+++ strauscpp3/ch_03_basics/iterators.cpp
@@ -175,8 +175,8 @@ int main(){}
 //а вот кстати фрактал :
 void frac(int k)
 {
-	float i, j, r, x, y = -16; while (puts(""), y++ < 15)
-		for (x = 0; x++ < 84; putchar(" .:-;!/>)|&IH%*#"[k & 15]))
+	float i, j, r, y = -16; while (puts(""), y++ < 15)
+		for (int x = 0; x++ < 84; putchar(" .:-;!/>)|&IH%*#"[k & 15]))
 			for (i = k = r = 0; j = r*r - i*i - 2 + x / 25, i = 2 * r*i + y / 10, j*j + i*i < 11 && k++ < 111; r = j);
 }
 

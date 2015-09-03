7803fea5c0bf68484ba496a89cb2d8528c263846
Yuriy Cherkasov <Yuri.Cherkasov@gamil.com>
2015-09-03 16:27:21 +0700

    qt solution - ch02

--- qt_shlee/ch_02_signal_slot/counter.h
+++ qt_shlee/ch_02_signal_slot/counter.h
@@ -1,1 +1,41 @@
 #pragma once
+
+#include <QObject>
+
+// * any Q_OBJECT class could have any number of signals and slots
+// * any signal could have any arguments
+// * signal could be connected to several slots
+// * slot could accept any number of different signals
+// * signal-slot could be connected-disconnected in any point of application
+// * when object is destroyed all signals are disconnected
+// * signal processing is a bit slower than callback
+// * signal could not be public/protected/private
+// * slot could be public/protected/private
+// * connection could be thread-safe. It is detected automatically
+// * blockSignals() lock sending signals by the object
+
+class Counter : public QObject
+{
+    Q_OBJECT
+
+public:
+    Counter();
+
+public slots:
+
+    // processing signals of incrementing value
+    void increment_slot();
+
+signals:
+    // signals do not have implementation in the class
+    // they processed by MOC in a separate *.cpp
+    
+    // custom signal for exiting application
+    void exit_me();
+
+    // custom signal with parameter
+    void counter_changed(int);
+
+private:
+    int value_;
+};
--- qt_shlee/ch_02_signal_slot/main.cpp
+++ qt_shlee/ch_02_signal_slot/main.cpp
@@ -1,10 +1,28 @@
 #include <QApplication>
+#include <QLabel>
 #include <QPushButton>
+#include "counter.h"
 
 int main(int argc, char **argv)
 {
     QApplication app (argc, argv);
-    QPushButton button (QStringLiteral("Hello world !"));
-    button.show();
+    QLabel lbl("0");
+    QPushButton btn("Add");
+    Counter counter;
+
+    lbl.show();
+    btn.show();
+
+    // on click button send a signal to counter
+    QObject::connect(&btn, SIGNAL(clicked()), &counter, SLOT(increment_slot()));
+
+    // on a custom signal from counter increment label value (standard Qt slot)
+    // Parameter types should be the same!
+    QObject::connect(&counter, SIGNAL(counter_changed(int)), &lbl, SLOT(setNum(int)));
+
+    // on a custom signal from counter exit application (standard Qt slot)
+    QObject::connect(&counter, SIGNAL(exit_me()), &app, SLOT(quit()));
+
+    // processing loop here
     return app.exec();
 }

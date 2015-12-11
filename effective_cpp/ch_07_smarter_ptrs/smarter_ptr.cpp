d4a57703f943ddb2727c0e7242ec0da76cb5faf3
Yuriy Cherkasov <yuri.cherkasov.accounts@gmail.com>
2015-12-11 13:03:44 +0700

    eldger ch 07

--- effective_cpp/ch_07_smarter_ptrs/smarter_ptr.cpp
+++ effective_cpp/ch_07_smarter_ptrs/smarter_ptr.cpp
@@ -1,114 +1,31 @@
-#include <iostream>
+#include <memory>
+#include "view.h"
+#include "view_grains.h"
+
+using namespace std;
+
+class view_events;
+class view_drawings;
 
 
-// Task 6.6 Satter 1
 
-// We want to implement smart pointer with correct deep copying semantic
 
-// We might want the following additional features
-// * template copying and assignment (for polymophic usage)
-// * copying semantic traits
 
-// traits class that could describe cloning semantic (also delete and other)
-template <typename T>
-struct value_pointer_traits{
-    static T* clone(const T* p) { 
-        return new T(*p); 
-    }
-};
+// Something similar is implemented in MS COM technology
+void show_interface_ptr(){
+    // object itself could be hidden
+    view my_view;
 
-template <typename T>
-class value_ptr{
-public:
+    std::unique_ptr<view_events> ev(my_view);
+    ev->event1();
+    ev->event2();
 
-    // construction and destruction - could be expanded by deleter traits
-    explicit value_ptr(T* p = nullptr) : _p(p){}
-
-    ~value_ptr(){
-        delete _p;
-    }
-
-    // copy and assignment
-
-    value_ptr(const value_ptr& other) : _p(create_from(other._p)){}
-
-    value_ptr& operator=(const value_ptr& other){
-        value_ptr temp(other);
-        swap(temp);
-        return *this;
-    }
-
-    void swap(value_ptr& other){
-        std::swap(_p, other._p);
-    }
-
-    // template copy and assignment (do not replace own copy and assignment)
-
-    template <typename U>
-    value_ptr(const value_ptr<U>& other) : _p(create_from(other._p)){}
-
-    template <typename U>
-    value_ptr& operator=(const value_ptr<U>& other) {
-        value_ptr temp(other);
-        swap(temp);
-        return *this;
-    }
-
-    // pointer semantic (const and non-const)
-    const T& operator*() const { 
-        return *_p; 
-    }
-    
-    const T* operator->() const { 
-        return _p; 
-    }
-
-    T& operator*() { 
-        return *_p; 
-    }
-    
-    T* operator->() { 
-        return _p; 
-    }
-
-protected:
-
-    // all classes we copy from are our friends!
-    template <typename U> friend class value_ptr;
-
-    // copy from other type value_ptr
-    template <typename U>
-    T* create_from(const U* p) const {
-        return p ? static_cast<T*>(value_pointer_traits<U>::clone(p)) : nullptr;
-    }
-
-private:
-    T* _p;
-};
-
-struct A{
-    int i;
-    A() :i(){}
-    virtual ~A(){}
-};
-
-struct B : public A{
-    int i;
-    B() :i(){}
-};
-
-void show_value_ptr(){
-    value_ptr<A> a1(new A);
-    value_ptr<A> a2(a1);
-    value_ptr<A> a3;
-    a3 = a1;
-
-    value_ptr<B> b1;
-    value_ptr<B> b2(a1);
-    b1 = a1;
+    std::unique_ptr<view_drawings> dr(my_view);
+    dr->drawing1();
+    dr->drawing2();
 }
 
 int main(){
-    show_value_ptr();
+    show_interface_ptr();
     return 0;
 }

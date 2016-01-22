abc9bf825597334c3c222c81a54fa58c7ffae656
Yuriy Cherkasov <yuri.cherkasov.accounts@gmail.com>
2016-01-22 19:49:07 +0700

    ch 31 base

--- strauscpp4/ch31_containers/main.cpp
+++ strauscpp4/ch31_containers/main.cpp
@@ -5,6 +5,11 @@ using namespace std;
 /*
 
 New features:
+* initializer_list constructors for containers (31.3.2)
+* Move semantics for containers (31.3.2)
+* A singly-linked list: forward_list (31.4.2)
+* Hashcontainers:unordered_map,unordered_multimap,unordered_set,andunordered_multiset (31.4.3)
+* emplace() operations for containers (31.3.6)
 
 Examples:
 
